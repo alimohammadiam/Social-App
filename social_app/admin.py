3b16693378e5f4e9563cb4c783e7298663caf0b1
Ali Mohammadi <ali0182mohammadi@gmail.com>
2025-02-04 00:32:51 +0330

    Creat User Model

--- social_app/admin.py
+++ social_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'phone']
+    fieldsets = [
+        ('Additional Information', {
+            'fields': [
+                'date_of_birth', 'bio', 'photo', 'job', 'phone',
+            ],
+        }),
+    ]
